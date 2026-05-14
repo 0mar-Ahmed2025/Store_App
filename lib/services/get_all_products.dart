b865c63279b073634cbfd9a9d2a87a4c71cba9b3
0mar-Ahmed2025 <0mar3hme7@gmial.com>
2026-05-14 07:15:47 +0300

    creat get all products service

--- lib/services/get_all_products.dart
+++ lib/services/get_all_products.dart
@@ -1,1 +1,20 @@
-class GetAllProducts {}
+// ignore_for_file: unused_local_variable
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:store_app/models/product_model.dart';
+
+class AllProductsService {
+  Future<List<ProductModel>> getAllProducts() async {
+    http.Response response = await http.get(
+      Uri.parse("https://fakestoreapi.com/products"),
+    );
+
+    List<dynamic> data = jsonDecode(response.body);
+
+    List<ProductModel> products = [];
+    for (int i = 0; i < data.length; i++) {
+      products.add(ProductModel.fromJson(data[i]));
+    }
+    return products;
+  }
+}
